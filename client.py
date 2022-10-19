f532422bd4c029dc37d0b5cd584faf92d7a033e0
Alala <raffaello@hotmail.de>
2022-10-19 08:38:10 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,2 +1,18 @@
 import socket
 
+ipadress = 'localhost'
+prot = 9999
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect((localhost, port))
+
+done = False
+
+while not done:
+  client.send(input('Message: ').encode('utf-8'))
+  msg = client.recv(1024).decode('utf-8')
+  if msg ==  'quit':
+    done = True
+  else:
+    print(msg)
